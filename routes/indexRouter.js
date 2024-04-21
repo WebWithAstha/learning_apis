31f052bb0f537181e93eaa089dd60e272bfa1644
WebWithAstha <lodhiastha85@gmail.com>
2024-04-21 21:55:08 +0530

    routes and respective controllers created

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express()
+const userModel = require('../models/userModel.js')
+const {home,register,allusers} = require('../controllers/indexController.js')
 
-router.get('/',function(req,res,next){
-    res.send('Hello World')
-})
+router.get('/', home)
+router.post('/register', register)
+router.get('/allusers', allusers)
+
 
 
 
